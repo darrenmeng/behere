53ea90c3f3515449c738717720f0e1b315570a1f
CP Wen <cpwen@CPdeMacBook-Air.local>
2015-06-22 17:38:36 +0800

    增加地圖應用的framework、view Controller、info.plist中的key值
    增加：
    1. 地圖要用的MapKit.framework
    2. 地圖要用的CoreLocation.framework
    3. 在storyboard中增加View Controller，並與AppleMapViewController連結
    4. 在View Controller中加入MapView
    
    修改：
    1. info.plist，增加NSLocationAlwaysUsageDescription

---
+++ AppleMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppleMapViewController.h
+//  beenhere
+//
+//  Created by CP Wen on 2015/6/22.
+//  Copyright (c) 2015年 MobileIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppleMapViewController : UIViewController
+
+@end
